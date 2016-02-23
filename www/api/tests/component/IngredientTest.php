2b9636ef204be92588ffab0a7595f1ee4b4dc2de
jordins <jordi.nistal@gmail.com>
2016-02-23 18:28:00 +0100

    Added some integration tests

---
+++ www/api/tests/component/IngredientTest.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testWhenPostingIngredientShouldReturnCorrectJson()
+    {
+        $this->post('/ingredient', ['name' => 'Roquefort'])
+             ->seeJson([
+                 "id"=>1, "name"=>"Roquefort"
+             ]);
+    }
+
+    public function testWhenDeletingIngredientShouldRemoveIt()
+    {
+        $this->insertIngredient('Roquefort');
+        $response = $this->call('DELETE', '/ingredient/1');
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function testWhenDeletingUnexistingIngredientShouldReturnError()
+    {
+        $this->insertIngredient('Roquefort');
+        $response = $this->call('DELETE', '/ingredient/7823');
+        $this->assertEquals(404, $response->status());
+    }
+
+
+    public function testWhenGettingAllIngredientsShouldReturnList()
+    {
+        $this->insertIngredient('Roquefort');
+        $this->insertIngredient('Xampinyons');
+        $this->get('/ingredient')
+                     ->seeJson([
+                         "id"=>1, "name"=>"Roquefort"
+                     ],[
+                         "id"=>2, "name"=>"Xampinyons"
+                     ]);
+    }
+
+
+    public function testWhenUpdatingAnIngredientShouldModifyItCorrectly()
+    {
+        $this->insertIngredient('Roquefort');
+        $this->put('/ingredient/1', ["name"=>"RoquefortModified"]);
+        $this->get('/ingredient/1')
+                    ->seeJson([
+                         "id"=>1, "name"=>"RoquefortModified"
+                     ]);
+    }
+
+
+    protected function insertIngredient ($name) {
+        $this->post('/ingredient', ['name' => $name]);
+    }
+
+}
